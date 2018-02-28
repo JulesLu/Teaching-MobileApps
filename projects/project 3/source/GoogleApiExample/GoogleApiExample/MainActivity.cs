bcc38e8d01cf700670b3f9ad1c1af824862fa14c
Julian <jjl438@humboldt.edu>
2018-02-27 22:46:19 -0800

    More updates, may have fixed bugs

--- projects/project 3/source/GoogleApiExample/GoogleApiExample/MainActivity.cs
+++ projects/project 3/source/GoogleApiExample/GoogleApiExample/MainActivity.cs
@@ -2,12 +2,10 @@
 using Android.Widget;
 using Android.OS;
 using Android.Content;
+using System;
 using System.Collections.Generic;
 using Android.Content.PM;
 using Android.Provider;
-using Uri = Android.Net.Uri;
-using Android;
-using System;
 
 namespace GoogleApiExample
 {
@@ -20,8 +18,9 @@ namespace GoogleApiExample
 
         public static Java.IO.File _file;
         public static Java.IO.File _dir;
+        Boolean success = false;
 
-        string thing; 
+
         protected override void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
@@ -71,6 +70,7 @@ namespace GoogleApiExample
             //FileProvider.GetUriForFile
 
             intent.PutExtra(MediaStore.ExtraOutput, Android.Net.Uri.FromFile(_file));
+            success = true;
             StartActivityForResult(intent, 0);
         }
 
@@ -89,33 +89,34 @@ namespace GoogleApiExample
             ImageView imageView = null;
 
             base.OnActivityResult(requestCode, resultCode, data);
-            //SetContentView(Resource.Layout.TakenPic);
+            SetContentView(Resource.Layout.TakenPic);
 
             // Display in ImageView. We will resize the bitmap to fit the display.
             // Loading the full sized image will consume too much memory
             // and cause the application to crash.
-            imageView = FindViewById<ImageView>(Resource.Id.takenPicture);
-            int height = Resources.DisplayMetrics.HeightPixels;
-            int width = 1024;
-            bitmap = _file.Path.LoadAndResizeBitmap(width, height);
 
-            
+
+
+            if (success == true)
+            {
                 Intent mediaScanIntent = new Intent(Intent.ActionMediaScannerScanFile);
                 var contentUri = Android.Net.Uri.FromFile(_file);
                 mediaScanIntent.SetData(contentUri);
                 SendBroadcast(mediaScanIntent);
 
-               //mageView = FindViewById<ImageView>(Resource.Id.takenPicture);
-                
-            
+               imageView = FindViewById<ImageView>(Resource.Id.takenPicture);
+                 imageView = FindViewById<ImageView>(Resource.Id.takenPicture);
+                 int height = Resources.DisplayMetrics.HeightPixels;
+                 int width = 1024;
+                 bitmap = _file.Path.LoadAndResizeBitmap(width, height);
+            }
 
             if (bitmap != null)
             {
-                SetContentView(Resource.Layout.TakenPic);
-                imageView = FindViewById<ImageView>(Resource.Id.takenPicture);
-                imageView.Visibility = Android.Views.ViewStates.Visible;
+                //SetContentView(Resource.Layout.TakenPic);
+                //imageView = FindViewById<ImageView>(Resource.Id.takenPicture);
                 imageView.SetImageBitmap(bitmap);
-                SetContentView(Resource.Layout.TakenPic);
+                imageView.Visibility = Android.Views.ViewStates.Visible;
             }
 
             else
@@ -130,57 +131,55 @@ namespace GoogleApiExample
 
             //AC: workaround for not passing actual files
             // Android.Graphics.Bitmap bitmap = (Android.Graphics.Bitmap)data.Extras.Get("data");
-            if (bitmap != null)
-            {
-                //convert bitmap into stream to be sent to Google API
-                string bitmapString = "";
-                using (var stream = new System.IO.MemoryStream())
+                if (bitmap != null)
                 {
-                    bitmap.Compress(Android.Graphics.Bitmap.CompressFormat.Jpeg, 0, stream);
+                    //convert bitmap into stream to be sent to Google API
+                    string bitmapString = "";
+                    using (var stream = new System.IO.MemoryStream())
+                    {
+                        bitmap.Compress(Android.Graphics.Bitmap.CompressFormat.Jpeg, 0, stream);
 
-                    var bytes = stream.ToArray();
-                    bitmapString = System.Convert.ToBase64String(bytes);
-                }
+                        var bytes = stream.ToArray();
+                        bitmapString = System.Convert.ToBase64String(bytes);
+                    }
 
-                //credential is stored in "assets" folder
-                string credPath = "google_api.json";
-                Google.Apis.Auth.OAuth2.GoogleCredential cred;
+                    //credential is stored in "assets" folder
+                    string credPath = "google_api.json";
+                    Google.Apis.Auth.OAuth2.GoogleCredential cred;
 
-                //Load credentials into object form
-                using (var stream = Assets.Open(credPath))
-                {
-                    cred = Google.Apis.Auth.OAuth2.GoogleCredential.FromStream(stream);
-                }
-                cred = cred.CreateScoped(Google.Apis.Vision.v1.VisionService.Scope.CloudPlatform);
+                    //Load credentials into object form
+                    using (var stream = Assets.Open(credPath))
+                    {
+                        cred = Google.Apis.Auth.OAuth2.GoogleCredential.FromStream(stream);
+                    }
+                    cred = cred.CreateScoped(Google.Apis.Vision.v1.VisionService.Scope.CloudPlatform);
 
-                // By default, the library client will authenticate 
-                // using the service account file (created in the Google Developers 
-                // Console) specified by the GOOGLE_APPLICATION_CREDENTIALS 
-                // environment variable. We are specifying our own credentials via json file.
-                var client = new Google.Apis.Vision.v1.VisionService(new Google.Apis.Services.BaseClientService.Initializer()
-                {
-                    ApplicationName = "Project3",
-                    HttpClientInitializer = cred
-                });
+                    // By default, the library client will authenticate 
+                    // using the service account file (created in the Google Developers 
+                    // Console) specified by the GOOGLE_APPLICATION_CREDENTIALS 
+                    // environment variable. We are specifying our own credentials via json file.
+                    var client = new Google.Apis.Vision.v1.VisionService(new Google.Apis.Services.BaseClientService.Initializer()
+                    {
+                        ApplicationName = "GoogleApiExample",
+                        HttpClientInitializer = cred
+                    });
 
-                //set up request
-                var request = new Google.Apis.Vision.v1.Data.AnnotateImageRequest();
-                request.Image = new Google.Apis.Vision.v1.Data.Image();
-                request.Image.Content = bitmapString;
+                    //set up request
+                    var request = new Google.Apis.Vision.v1.Data.AnnotateImageRequest();
+                    request.Image = new Google.Apis.Vision.v1.Data.Image();
+                    request.Image.Content = bitmapString;
 
-                //tell google that we want to perform label detection
-                request.Features = new List<Google.Apis.Vision.v1.Data.Feature>();
-                request.Features.Add(new Google.Apis.Vision.v1.Data.Feature() { Type = "LABEL_DETECTION" });
-                var batch = new Google.Apis.Vision.v1.Data.BatchAnnotateImagesRequest();
-                batch.Requests = new List<Google.Apis.Vision.v1.Data.AnnotateImageRequest>();
-                batch.Requests.Add(request);
+                    //tell google that we want to perform label detection
+                    request.Features = new List<Google.Apis.Vision.v1.Data.Feature>();
+                    request.Features.Add(new Google.Apis.Vision.v1.Data.Feature() { Type = "LABEL_DETECTION" });
+                    var batch = new Google.Apis.Vision.v1.Data.BatchAnnotateImagesRequest();
+                    batch.Requests = new List<Google.Apis.Vision.v1.Data.AnnotateImageRequest>();
+                    batch.Requests.Add(request);
 
-                //send request.  Note that I'm calling execute() here, but you might want to use
-                //ExecuteAsync instead
-                var apiResult = client.Images.Annotate(batch).Execute();
-                thing = apiResult.Responses[0].LabelAnnotations[0].Description;
-                FindViewById<TextView>(Resource.Id.yourpic).Text += thing;
-                SetContentView(Resource.Layout.TakenPic);
+                    //send request.  Note that I'm calling execute() here, but you might want to use
+                    //ExecuteAsync instead
+
+                    var apiResult = client.Images.Annotate(batch).Execute(); 
 
             }
                 // Dispose of the Java side bitmap.
